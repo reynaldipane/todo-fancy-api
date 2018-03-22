3c468cfc9610e0bda056d9d0727f391d4003486a
Reynaldi Ananda Pane <reynaldipane@gmail.com>
2018-03-22 14:29:05 +0700

    Release 1

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const cors            = require('cors')
 const app             = express();
 
 require('dotenv').config()
-
-mongoose.connect('mongodb://localhost:27017/todofancy')
+mongoose.connect('mongodb://reynaldipane:12345@ds113749.mlab.com:13749/todofancy')
+// mongoose.connect('mongodb://localhost:27017/todofancy')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/todos.js
+++ routes/todos.js
@@ -3,10 +3,14 @@ const router         = express.Router();
 const todoController = require('../controllers/ToDoController')
 const auth           = require('../middlewares/auth')
 
-router.get('/', auth.check, todoController.findAll)
+router.get('/', todoController.findAll)
+router.post('/translate', todoController.translate)
+router.get('/:id', auth.check, todoController.findById)
 router.post('/create', auth.check, todoController.create)
 router.put('/update/:id', auth.check, todoController.update)
-router.delete('/delete/:id', auth.check, todoController.delete)
+router.put('/updatetask/:id', auth.check, todoController.updateStatusTask)
+router.delete('/delete/:id',  auth.check, todoController.delete)
 router.post('/search/:userid', auth.check, todoController.userSearch)
+router.post('/findbyuserid', todoController.findByUserId)
 
 module.exports = router;
